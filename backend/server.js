41b76a01eb073b655283ca81367cf9c4b50dba5e
swati-kashyap22 <kashyapswati0909@gmail.com>
2026-05-23 00:24:17 +0530

    Day 1 setup completed

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const upload = multer({ dest: "uploads/" });
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
+
+app.post("/upload-audio", upload.single("audio"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({
+      message: "No audio uploaded",
+    });
+  }
+
+  res.json({
+    message: "Audio uploaded successfully",
+    file: req.file,
+  });
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
