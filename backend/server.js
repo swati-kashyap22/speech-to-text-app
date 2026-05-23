bb77385433d090c3e8936f01522793a35d38a190
swati-kashyap22 <kashyapswati0909@gmail.com>
2026-05-23 15:50:24 +0530

    Day 2 backend packages and database setup started

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
 
 const app = express();
 
